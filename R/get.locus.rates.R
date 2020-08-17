681b580d626c68d4db57599a5d4ee91e8e7cf346
duchene <duchenedavid@gmail.com>
2020-08-17 13:26:52 +1000

    bug fixed

--- R/get.locus.rates.R
+++ R/get.locus.rates.R
@@ -1,5 +1,5 @@
-get.locus.rates <- function(locustr, sptr, branch.support.threshold = 0){
+get.locus.rates <- function(locustr, sptr, branch.support.threshold = 0, branch.length.threshold = 1e-6){
 	if(is.rooted(locustr)) locustr <- unroot(locustr)
-	rates <- sapply(1:Nedge(sptr), function(x) branch.present(locus.tree = locustr, sp.tree = sptr, sptredge = x, branch.support.threshold = branch.support.threshold))
+	rates <- sapply(1:Nedge(sptr), function(x) branch.present(locus.tree = locustr, sp.tree = sptr, sptredge = x, branch.support.threshold = branch.support.threshold, branch.length.threshold = branch.length.threshold))
 	return(rates)
 }
--- R/get.sptree.rates.R
+++ R/get.sptree.rates.R
@@ -1,10 +1,10 @@
 # Replace all branches in gene tree with those in species tree where present.
 
-get.sptree.rates <- function(locustr, sptr, branch.support.threshold = 0){
+get.sptree.rates <- function(locustr, sptr, branch.support.threshold = 0, branch.length.threshold = 1e-6){
 	if(is.rooted(locustr)) locustr <- unroot(locustr)
 	if(is.rooted(sptr)) sptr <- unroot(sptr)
 
-	rates <- sapply(1:nrow(locustr$edge), function(x) branch.present(locus.tree = sptr, sp.tree = locustr, sptredge = x, branch.support.threshold = branch.support.threshold))
+	rates <- sapply(1:nrow(locustr$edge), function(x) branch.present(locus.tree = sptr, sp.tree = locustr, sptredge = x, branch.support.threshold = branch.support.threshold, branch.length.threshold = branch.length.threshold))
 
 	locustr$edge.length[!is.na(rates)] <- rates[!is.na(rates)]	
 	
