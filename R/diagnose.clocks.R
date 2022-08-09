b5a9e6cc9afa6bd0966967d262c7193bbc5c3997
duchene <duchenedavid@gmail.com>
2022-08-09 11:04:31 +0200

    updated wrapper function

--- R/diagnose.clocks.R
+++ R/diagnose.clocks.R
@@ -1,6 +1,6 @@
 diagnose.clocks <- function(loctrs, sptr, sp.time.tree = T, branch.support.threshold = 0, branch.length.threshold = 5e-6, log.branches = F, pca = T, mds = F, pca.permutations = 100, mean.scaling.brlen = 0.05, ncore = 1, sammon.correction = F, clustering.boot.samps = 50, kmax = 10, other.data = NULL, pdf.file = "clock.diagnosis"){
 	cat("Collecting clocks", fill = T)
-	crx <- collect.clocks(loctrs, sptr, sp.time.tree = T, branch.support.threshold, branch.length.threshold, verbose = F)
+	crx <- collect.clocks(loctrs, sptr, sp.time.tree, branch.support.threshold, branch.length.threshold, verbose = F)
 	cat("Creating representation of clock space", fill = T)
 	crx <- clock.space(crx, sptr, pca, mds, sp.time.tree, log.branches, pca.permutations, mean.scaling.brlen, ncore, sammon.correction, verbose = F, pdf.file)
 	cat("Grouping clocks", fill = T)
