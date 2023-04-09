e855e3500b0256cf5a185aaade4d3931788ff7f0
David Alejandro Duchene Garzon <duchenedavid@gmail.com>
2023-04-09 16:28:31 +0200

    updated show method

--- R/clockstarx.class.R
+++ R/clockstarx.class.R
@@ -10,12 +10,12 @@ setMethod("print",
 		if(x$pca.clock.space$pPCs[1] < 0.01) cat("Significant PCs of relative (raw) rates include:\n") else cat("No PCs significantly describe variation in relative (raw) rates.\n")
 		for(i in 1:length(x$pca.clock.space$pPCs)){
 			if(x$pca.clock.space$pPCs[i] > 0.01) break
-			cat(paste0("PC", i, " with ", length(which(x$pca.clock.space$pPCs[i] < 0.01)), " significant branches.\n"))
+			cat(paste0("PC", i, " with ", length(which(x$pca.clock.space$pIL[,i] < 0.01)), " significant branches.\n"))
 		}
 		if(x$weighted.pca.clock.space$pPCs[1] < 0.01) cat("Significant PCs of relative (raw) rates include:\n") else cat("No PCs significantly describe variation in residual (weighted) rates.\n")
 		for(i in 1:length(x$weighted.pca.clock.space$pPCs)){
                         if(x$weighted.pca.clock.space$pPCs[i] > 0.01) break
-                        cat(paste0("PC", i, " with ", length(which(x$weighted.pca.clock.space$pPCs[i] < 0.01)), " significant branches.\n"))
+                        cat(paste0("PC", i, " with ", length(which(x$weighted.pca.clock.space$pIL[,i] < 0.01)), " significant branches.\n"))
                 }
 		cat("The full results of tests of clocks are found in the elements pPhi, pPsi, pPCs, and pIL in the pca.clock.space objects.\n")
 	}
@@ -31,12 +31,12 @@ setMethod("show",
                 if(x$pca.clock.space$pPCs[1] < 0.01) cat("Significant PCs of relative (raw) rates include:\n") else cat("No PCs significantly describe variation in relative (raw) rates.\n")
                 for(i in 1:length(x$pca.clock.space$pPCs)){
                         if(x$pca.clock.space$pPCs[i] > 0.01) break
-                        cat(paste0("PC", i, " with ", length(which(x$pca.clock.space$pPCs[i] < 0.01)), " significant branches.\n"))
+                        cat(paste0("PC", i, " with ", length(which(x$pca.clock.space$pIL[,i] < 0.01)), " significant branches.\n"))
                 }
                 if(x$weighted.pca.clock.space$pPCs[1] < 0.01) cat("Significant PCs of relative (raw) rates include:\n") else cat("No PCs significantly describe variation in residual (weighted) rates.\n")
                 for(i in 1:length(x$weighted.pca.clock.space$pPCs)){
                         if(x$weighted.pca.clock.space$pPCs[i] > 0.01) break
-                        cat(paste0("PC", i, " with ", length(which(x$weighted.pca.clock.space$pPCs[i] < 0.01)), " significant branches.\n"))
+                        cat(paste0("PC", i, " with ", length(which(x$weighted.pca.clock.space$pIL[,i] < 0.01)), " significant branches.\n"))
                 }
                 cat("The full results of tests of clocks are found in the elements pPhi, pPsi, pPCs, and pIL in the pca.clock.space objects.\n")
         }
